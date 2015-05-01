dcdf108a3a37dcb9dc5b14550af41111b778da41
Keshav Patel <patel15k2@ncssm.edu>
2015-05-01 10:34:20 -0400

    program can pull cities and data out

--- js/dataCrunch.js
+++ js/dataCrunch.js
@@ -1,15 +1,38 @@
 var fs = require("fs")
 
 var content;
-fs.readFile('../cleanData.csv', encoding="UTF-8", function read(err, data) {
-    if (err) {
+var values = {};
+fs.readFile('../cleanData.csv', encoding="UTF-8", function read(err, data) 
+{
+    if (err) 
+    {
         throw err;
     }
     content = data;
     process();
+    console.log(values);
 });
 
-function process(){
-    for(var k = 0; k < content.length; k++){
-    content[0]
+function process()
+{
+    var start = 0;
+    for(var k = 0; k < content.length; k++)
+    {
+        if(content[k] === "\"" && content[k+1] === ",")
+        {
+            var id = content.substring(start+1, k);
+            var num = "";
+            var j = 2;
+            while(content[k+j] != ",")
+            {
+                num += content[k+j];
+                j++;
+            }
+            values[id] = num;
+        }
+        else if(content[k] === "\"")
+        {
+            start = k;
+        }
+    }
 }
