c427455501d64f8179e06e5d038fa3eb84b87799
Ebube Chuba <chuba15e@ncssm.edu>
2015-05-11 15:12:38 -0400

    changed HTML and JS for cool AJAX features

--- js/magic.js
+++ js/magic.js
@@ -1,3 +1,8 @@
  $(document).ready(function() {
     $('#map').usmap({});
 });
+
+$("#start").click(function() {
+    var toLoad = "map/index.html"
+    $(document.getElementsByName("main")).hide('slide', {direction: 'left'}, fast)
+});
