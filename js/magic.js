0b428639bf7a92ca3a0dab8bf85492fc07d3f9bf
Ebube Chuba <chuba15e@ncssm.edu>
2015-05-12 11:06:48 -0400

    start button causes page to slide out. AJAX coming soon

--- js/magic.js
+++ js/magic.js
@@ -1,8 +1,8 @@
  $(document).ready(function() {
     $('#map').usmap({});
-});
-
-$("#start").click(function() {
-    var toLoad = "map/index.html"
-    $(document.getElementsByName("main")).hide('slide', {direction: 'left'}, fast)
+    
+    $("#start").click(function() {
+        var toLoad = "map/index.html";
+        $(document.getElementsByTagName("main")[0]).hide('slide', {direction: 'left'}, 'slow')
+    });
 });
