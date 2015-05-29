3a1e89073953895fbb73b67d2c5ec4bf9cff2b75
Ebube Chuba <chuba15e@ncssm.edu>
2015-05-29 16:48:23 -0400

    added start button functionality? need to fix load error

--- js/magic.js
+++ js/magic.js
@@ -1,20 +1,31 @@
- $(document).ready(function() {
-    $("#start").click(function() {
+ $(document).ready(function()
+ {
+    $("#start").click(function()
+    {
         var toLoad = "map/index.html #content";
         $("#content").hide('slide', {direction: 'left'}, 'slow');
         $("#load").fadeIn('normal');
         setTimeout(loadContent, 800);
-        function hideLoader() {
+        function hideLoader()
+        {
             $("#load").fadeOut('normal');
         }
-        function loadContent() {
+        function loadContent()
+        {
             setTimeout(2000);
             $("#content").load(toLoad, '', showNewContent());
         }
-        function showNewContent() {
+        window.location.href = window.location.href.substring(0, window.location.href.length - 10) + "map/index.html";
+        function showNewContent()
+        {
             hideLoader();
             $("#content").show('slide', {direction: 'right'}, 'slow');
 //            $("#map")[0].usmap({});
         }
+        function doLogic()
+        {
+            return false;
+        }
+        return doLogic;
     });
 });
